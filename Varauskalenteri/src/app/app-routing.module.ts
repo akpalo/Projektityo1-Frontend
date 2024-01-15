d1cc634a4535e575c73e997874b451638e9d13f5
akpalo <akseli.palo@student.lab.fi>
2024-01-15 11:01:12 +0200

    Varaalaitemoduulit pistettu

--- Varauskalenteri/src/app/app-routing.module.ts
+++ Varauskalenteri/src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
-import { VaraaLaiteComponent } from './varaa-laite/varaa-laite.component';
 import { KalenteriComponent } from './kalenteri/kalenteri.component';
 import { AngCalendarComponent } from './ang-calendar/ang-calendar.component';
 
@@ -10,9 +9,8 @@ const routes: Routes = [
   { path: "kalenteri", component: KalenteriComponent},
   { path: "login", component: LoginComponent },
   { path: "logout", component: LogoutComponent },
-  { path: "varaa", component: VaraaLaiteComponent },
   { path: "angCalendar", component: AngCalendarComponent },
-  { path: '', redirectTo: '/angCalendar', pathMatch: 'full' },
+  { path: '', redirectTo: '/kalenteri', pathMatch: 'full' },
 ];
 
 @NgModule({
